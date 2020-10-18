d46815848598e77b4a052de1bedd447ca7d5ff3e
Tuyen <kieu.thi.tuyen@sun-asterisk.com>
2020-10-18 23:00:07 +0700

    add new function

--- autoLogin/autoShopee.py
+++ autoLogin/autoShopee.py
@@ -28,7 +28,7 @@ homePage = accountFile.readline()
 
 # you are using firefox or chrome?
 options = Options()
-#options.add_argument("user-data-dir=C:\\Users\\ducky\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+# options.add_argument("user-data-dir=C:\\Users\\ducky\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
 options.add_argument("user-data-dir=C:\\Users\\ducky\\AppData\\Local\\Mozilla\\Firefox\\Profiles\\Default User")
 
 driver = selenium.webdriver.Firefox(executable_path=pathFirefoxDriver)
@@ -52,29 +52,35 @@ try:
             loginLink = driver.find_element_by_link_text("Đăng Nhập")
             loginLink.click()
             # enter userName and password
+            time.sleep(2)
             userE = driver.find_element(By.NAME, "loginKey").send_keys(username)
             passE = driver.find_element(By.NAME, "password").send_keys(password)
             # click button login
             btnLogin = driver.find_element_by_css_selector('button._35rr5y._32qX4k._1ShBrl._3z3XZ9._2iOIqx._2h_2_Y')
-            WebDriverWait(driver, 2).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "button._35rr5y._32qX4k._1ShBrl._3z3XZ9._2iOIqx._2h_2_Y")))
+            WebDriverWait(driver, 2).until(ec.visibility_of_element_located(
+                (By.CSS_SELECTOR, "button._35rr5y._32qX4k._1ShBrl._3z3XZ9._2iOIqx._2h_2_Y")))
             close = driver.find_element_by_css_selector("button._35rr5y._32qX4k._1ShBrl._3z3XZ9._2iOIqx._2h_2_Y")
             actionChains = ActionChains(driver)
             actionChains.double_click(close).perform()
 
             # close popup windows after login successful (a popup of shopee home page)
-            WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, "shopee-popup__close-btn")))
+            WebDriverWait(driver, 10).until(
+                ec.visibility_of_element_located((By.CLASS_NAME, "shopee-popup__close-btn")))
             close = driver.find_element_by_class_name("shopee-popup__close-btn")
             actionChains = ActionChains(driver)
             actionChains.double_click(close).perform()
 
-            WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, "shopee-skinny-banner__full-height.V1Fpl5")))
+            WebDriverWait(driver, 10).until(
+                ec.visibility_of_element_located((By.CLASS_NAME, "shopee-skinny-banner__full-height.V1Fpl5")))
             deal0Vnd = driver.find_element_by_class_name("shopee-skinny-banner__full-height.V1Fpl5")
             actionChains = ActionChains(driver)
             actionChains.double_click(deal0Vnd).perform()
 
+            time.sleep(1)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-            WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, "_3IGAiu._1aqQcY")))
-            layMa = driver.find_element_by_class_name("_3IGAiu._1aqQcY")
+            WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, "button._1_K8ji")))
+            layMa = driver.find_element_by_css_selector("button._1_K8ji")
             actionChains = ActionChains(driver)
             for ma in layMa:
                 actionChains.double_click(ma).perform()
